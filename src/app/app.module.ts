97c9d8a7824104b248176d727ab4acb1f209934d
Mitchel Delachevrotiere <mdelachevrotiere@intouchinsight.com>
2023-02-25 16:38:56 -0500

    feat:: add Auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AuthComponent, ProfileComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: environment.AUTH_DOMAIN,
+      clientId: environment.AUTH_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  AUTH_DOMAIN: 'AUTH_DOMAIN',
+  AUTH_CLIENT_ID: 'AUTH_CLIENT_ID',
+};
